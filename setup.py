3f81f0b9d252f7c6558d3f052ce3c0b914cb2fa5
Atez-alter <57753690+Zeta-qixi@users.noreply.github.com>
2023-03-29 17:24:41 +0800

    0.5.6

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import setup
 requirements = [r.strip() for r in open("requirements.txt", 'r', encoding='utf-8').readlines()]
 
 setup(
-    name='nonebot-plugin-unoconv',
-    version='0.5.0',
+    name='nonebot-plugin-clock',
+    version='0.5.6',
     author='Zeta',
     author_email='',
     long_description="https://github.com/Zeta-qixi/nonebot-plugin-clock",
