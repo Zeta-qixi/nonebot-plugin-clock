92fe0c4cef6f3ecbe8ba76ec9e94d35f04b6a280
Zeta-qixi <1019289695@qq.com>
2026-02-28 12:25:42 +0800

    修改插件信息

--- nonebot_plugin_clock/__init__.py
+++ nonebot_plugin_clock/__init__.py
@@ -8,5 +8,8 @@ __plugin_meta__ = PluginMetadata(
     name="nonebot_plugin_clock",
     description="QQ机器人智能闹钟插件，支持群聊与私聊提醒、支持图片发送、自然语言设置、多样化时间格式",
     usage="添加闹钟 21:00 | 查看闹钟 | 删除闹钟 1 | 每天8点叫我起床",
+    type="application",
     config=Config,
+    homepage="https://github.com/Zeta-qixi/nonebot-plugin-clock",
+    supported_adapters=["~onebot.v11"],
 )
