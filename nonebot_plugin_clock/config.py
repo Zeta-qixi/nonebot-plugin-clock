42fb3a8b468ff5c9536c287851d5122fff75d5a5
Zeta-qixi <1019289695@qq.com>
2026-02-28 11:23:39 +0800

    UPDATE

--- nonebot_plugin_clock/config.py
+++ nonebot_plugin_clock/config.py
@@ -5,14 +5,13 @@ from nonebot import require, get_plugin_config
 require("nonebot_plugin_localstore")
 import nonebot_plugin_localstore as store
 
-
 class Config(BaseModel):
+    clock_db_path: Path  = Field(default=store.get_plugin_data_file("data.db"))
     group_at_me: bool = Field(default=True)
-    use_llm: bool = Field(default=False)
+    use_llm: bool = Field(default=True)
 
 clock_config = get_plugin_config(Config)
 AT_ME = clock_config.group_at_me
-USE_LLM = clock_config.use_llm
-
-DB_PATH = store.get_plugin_data_file("data.db")
+DB_PATH = clock_config.clock_db_path
 IMAGE_DIR = store.get_plugin_data_file('images')
+USE_LLM = clock_config.use_llm
